9b4fd44fdfb319a4778576bd0fd4e813621ddf83
Muthuswamy23 <muthuswamy323@gmail.com>
2025-08-09 16:03:39 +0530

    new file python.py is created & ubtated new.txt

---
+++ python.py
@@ -0,0 +1,22 @@
+# opening file in read mode
+#file=open("c:\\Users\\ELCOT\\Documents\\RMgit\\githubms2\\new.txt","r")
+#content=file.read()
+#print(content)
+#file.close
+
+#openig in write mode
+file=open("c:\\Users\\ELCOT\\Documents\\RMgit\\githubms2\\new.txt","w")
+file.write("\n Hello this is Muthuswamy \n")
+file.write("This is the new line added")
+file.close()
+
+#appending 
+file=open("c:\\Users\\ELCOT\\Documents\\RMgit\\githubms2\\new.txt","a")
+file.write("\nif we open the file in write mode the old datas are been deleted\n")
+file.write("but if we open in append mode it won't delete the old file")
+file.close
+
+file = open("c:\\Users\\ELCOT\\Documents\\RMgit\\githubms2\\new.txt", "r")
+for line in file:
+    print(line.strip())  # strip removes \n
+file.close()
